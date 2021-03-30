bfc42d5ef9ce164d9ae6b583f335494c41f552ad
idembele70 <idembele70@gmail.com>
2021-03-30 21:18:44 +0200

    notre premier composant partie 1

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-let n = 0;
-
-function numberFormat(n) {
-    return n.toString().padStart(2, '0')
+function Welcome(props) {
+    console.log(props.name);
+    return <h1>Bonjour {props.name}</h1>;
 }
 
-function render() {
-    const items = [
-        'Tache 1',
-        'Tache 2',
-        'Tache 3'
-    ];
-    const lis = items.map(item=><li>{item}</li>)
-    const title = <React.Fragment>
-         <h1 className='title' id={"title" + n}>
-            Bonjour les gens &nbsp;
-        <span>{(n)}</span>
-        </h1>
-        <ul>
-            {items.map((item,k)=><li key={k}>{item}</li>)}
-        </ul>
-    </React.Fragment>
-
-    ReactDOM.render(title, document.getElementById('app'))
-}
-
-render()
-
-window.setInterval(() => {
-    n++
-    render()
-}, 1000);
+ReactDOM.render(<Welcome name = "Jean" />, document.querySelector('body > div'));

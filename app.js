a42763b082b2a25f10329f5f7dada9f93d41672f
idembele70 <idembele70@gmail.com>
2021-05-07 23:14:15 +0200

    TP celsius / fahrenheit ma version !

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-class Field extends React.Component {
-    render() {
-        const { children, name, value, onChange, type } = this.props;
-        return <div className="form-group">
-            <label htmlFor={name}>{children}</label>
-            {(type == "text") ?
-                <input type={type} value={value} onChange={onChange} id={name} name={name} className="form-control" /> :
-                <input type={type} checked={value} onChange={onChange} id={name} name={name} />
-            }
-        </div>
-    }
-};
-
-function CheckBox({ children, val, onChange, name, type }) {
-    return <div className="form-group">
-        <input type={type} checked={val} onChange={onChange} id={name} name={name} />
-        <label>{children}</label>
-    </div>
+// functions :
+function Field({ children, name, onChange, value }) {
+    return <fieldset>
+        <legend>{children}</legend>
+        <input type="text" id={name} name={name} value={value} onChange={onChange} />
+    </fieldset>
 }
 
-class Home extends React.Component {
+// class
+
+class Temperature extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
-            nom: "",
-            prenom: "",
-            newsletter: false,
+            celcius: '',
+            farenheit: '',
         }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.temperatureCelcius = this.temperatureCelcius.bind(this);
+        this.temperaturefarenheit = this.temperaturefarenheit.bind(this);
     }
-    handleChange(e) {
-        const name = e.target.name;
-        const type = e.target.type;
-        const value = type === "checkbox" ? e.target.checked : e.target.value;
-        return this.setState({ [name]: value })
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        const data = JSON.stringify(this.state);
+
+    temperatureCelcius(e) {
+        const celcius = parseInt(e.target.value)
+        const farenheit = (celcius * 9 / 5) + 32
         this.setState({
-            nom: '',
-            prenom: '',
-            newsletter: false
+            celcius: celcius ? celcius : '',
+            farenheit: farenheit ? farenheit : ''
+        })
+    }
+    temperaturefarenheit(e) {
+        const farenheit = parseInt(e.target.value);
+        const celcius = farenheit * 9 / 5 + 32;
+        this.setState({
+            farenheit: farenheit ? farenheit : '',
+            celcius: celcius ? celcius : '',
         })
     }
     render() {
-        return <form className="container" onSubmit={this.handleSubmit} >
-            <Field type="text" name="nom" value={this.state.nom} onChange={this.handleChange}>Nom</Field>
-            <Field type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange}>Prénom</Field>
-            <CheckBox type="checkbox" name="newsletter" id="newsletter" onChange={this.handleChange} checked={this.state.newsletter}/> <br />
-            <button className="btn btn-primary m-1">Envoyer</button>
-            {JSON.stringify(this.state)}
-        </form>
-    }
+        return <div>
+            <Field type="text" name="celcius" onChange={this.temperatureCelcius} value={this.state.celcius} >Enter temperature in Celcius</Field>
+            <Field type="text" name="fahrenheit" onChange={this.temperaturefarenheit} value={this.state.farenheit} >Enter temperature in Farenheit</Field>
+            <p>{this.state.celcius && this.state.celcius > 100 ? "The water would boil" :
+                this.state.celcius && this.state.celcius < 100 ? "The vater would not boil" : ''} </p>
+        </div>
+    } 
 };
 
-ReactDOM.render(<Home />, document.body.firstElementChild);
+
+
+
+const Home = () => {
+    return <React.Fragment>
+        <Temperature />
+    </React.Fragment>
+}
+document.body.style.background = "#00000073"
+
+ReactDOM.render(<Home />, document.body.children[0]);

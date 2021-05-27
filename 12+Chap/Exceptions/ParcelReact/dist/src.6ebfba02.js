d9d1f4976885afb089c1374c15103047dc2d8c19
idembele70 <idembele70@gmail.com>
2021-05-27 23:11:50 +0200

    uselayoutEffect

--- 12+Chap/Exceptions/ParcelReact/dist/src.6ebfba02.js
+++ 12+Chap/Exceptions/ParcelReact/dist/src.6ebfba02.js
@@ -29569,17 +29569,65 @@ if ("development" === 'production') {
 },{"./cjs/react-dom.development.js":"node_modules/react-dom/cjs/react-dom.development.js"}],"src/index.jsx":[function(require,module,exports) {
 "use strict";
 
+var _react = _interopRequireWildcard(require("react"));
+
 var _reactDom = require("react-dom");
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr && (typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]); if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 // racourci JS
 var _document = document,
     body = _document.body;
 body.style.background = "#00000059";
 
-function App() {}
+function wait(duration) {
+  var time = Date.now();
 
-(0, _reactDom.render)( /*#__PURE__*/React.createElement(App, null), body.querySelector("#app"));
-},{"react-dom":"node_modules/react-dom/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  while (duration > Date.now() - time) {
+    ;
+  }
+}
+
+function App() {
+  var _useState = (0, _react.useState)(0),
+      _useState2 = _slicedToArray(_useState, 2),
+      count = _useState2[0],
+      setCount = _useState2[1];
+
+  var refBtn = (0, _react.useRef)(0);
+  var onIncremente = (0, _react.useCallback)(function () {
+    setCount(function (c) {
+      return c + 1;
+    });
+  }, []);
+  (0, _react.useEffect)(function () {
+    wait(1000);
+    refBtn.current.style.color = count % 2 === 0 ? "red" : "green";
+    console.log("in");
+  }, [count]);
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("button", {
+    type: "submit",
+    onClick: onIncremente,
+    ref: refBtn
+  }, count));
+}
+
+(0, _reactDom.render)( /*#__PURE__*/_react.default.createElement(App, null), body.querySelector("#app"));
+},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -29607,7 +29655,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56584" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54267" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- 12+Chap/Exceptions/ParcelReact/src/index.jsx
+++ 12+Chap/Exceptions/ParcelReact/src/index.jsx
@@ -1,13 +1,39 @@
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { render } from "react-dom";
 
 // racourci JS
 const { body } = document;
 body.style.background = "#00000059";
 
-
-
+function wait(duration) {
+  const time = Date.now();
+  while (duration > Date.now() - time);
+}
 function App() {
- 
+  const [count, setCount] = useState(0);
+  const refBtn  = useRef(0);
+  const onIncremente = useCallback(() => {
+    setCount((c) => c + 1);
+  }, []);
+  useEffect(() => {
+   wait(1000);
+   refBtn.current.style.color = (count % 2 === 0 ? "red" : "green")
+   console.log("in");
+  }, [count]);
+  return (
+    <>
+      <button type="submit" onClick={onIncremente} ref={refBtn} >
+        {count}
+      </button>
+    </>
+  );
 }
 
 render(<App />, body.querySelector("#app"));
